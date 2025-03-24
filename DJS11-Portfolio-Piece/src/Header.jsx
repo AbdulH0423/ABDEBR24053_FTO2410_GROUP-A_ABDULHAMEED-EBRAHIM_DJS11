6ea8b15350e15312c1b096b0b1719615f9465741
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-03-24 05:35:21 +0200

    Created basic header component

---
+++ DJS11-Portfolio-Piece/src/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Podacast</h1>
+      <nav>
+        <ul>
+            <li>
+                <img src="" alt="Logo"></img>
+            </li>
+          <li>
+            <a href="">
+                <svg></svg>
+            </a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

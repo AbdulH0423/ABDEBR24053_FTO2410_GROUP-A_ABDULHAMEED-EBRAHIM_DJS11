36f17b64c52d0d9ceb93f68d887ccd047c81cf0d
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-04-08 12:04:52 +0200

    Added new components into layout

--- DJS11-Portfolio-Piece/src/App.jsx
+++ DJS11-Portfolio-Piece/src/App.jsx
@@ -1,8 +1,22 @@
-import Header from "./Header"
+import PodcastFooter from "./Components/GlobalComponents/PodcastFooter";
+import PodcastHeader from "./Components/GlobalComponents/PodcastHeader";
+// import PodcastHeader from "./Components/GlobalComponents/PodcastFooter"
+
 function App() {
-    <>
-    <Header />
-    </>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <PodcastHeader />
+
+      <main className="flex-1 p-4">
+        <h1 className="text-2xl font-bold">Welcome to the Podcast App</h1>
+        <p className="mt-2 text-gray-700">
+          This is a simple podcast application built with React and Tailwind CSS.
+        </p>
+        {/* Add more content here */}
+      </main>
+      <PodcastFooter />
+    </div>
+  );
 }
 
-export default App
+export default App;

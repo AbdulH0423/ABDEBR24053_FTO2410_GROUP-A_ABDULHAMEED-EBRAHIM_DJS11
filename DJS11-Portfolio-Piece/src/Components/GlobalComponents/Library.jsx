315c5237be211cf87f5e2b7ac9c6a4a9338c2da6
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-04-14 16:23:19 +0200

    Added loading and error state logic to library

--- DJS11-Portfolio-Piece/src/Components/GlobalComponents/Library.jsx
+++ DJS11-Portfolio-Piece/src/Components/GlobalComponents/Library.jsx
@@ -35,6 +35,37 @@ function Library(){
         
     }, []);
 
+    
+
+    if (loading) {
+        return (
+            <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {Array.from({ length: 12 }).map((_, i) => (
+            <div
+          key={i}
+            className="animate-pulse bg-white shadow rounded-lg p-4 space-y-4"
+        >
+          <div className="h-4 bg-gray-400 rounded w-full"></div>
+          <div className="h-3 bg-gray-300 rounded w-full"></div>
+          <div className="h-3 bg-gray-200 rounded w-full"></div>
+            </div>
+      ))}
+    </div>
+          );
+   
+    }
+
+    if (error) {
+        return (
+            <div className="p-4 text-center text-red-500">
+                <h1 className="text-5xl font-extrabold">{error}</h1>
+            </div>
+        );
+    }
+
+    return (
+        <div className="grid grid-cols-1 gap-4 p-4">
+        {podcasts.map((podcast) => (
             <div key={podcast.id} className="bg-white rounded-lg shadow p-4 cursor-pointer hover:bg-gray-300 transition duration-200" 
             >
 

8228d11797844d33d186eaec46ae6bde7d0e925f
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-04-15 16:51:23 +0200

    Added updated component props to APP

--- DJS11-Portfolio-Piece/src/App.jsx
+++ DJS11-Portfolio-Piece/src/App.jsx
@@ -1,18 +1,27 @@
 import PodcastFooter from "./Components/GlobalComponents/PodcastFooter";
 import PodcastHeader from "./Components/GlobalComponents/PodcastHeader";
 import Library from "./Components/GlobalComponents/Library";
-/* import LoadingFooter from "./Components/Loading/LoadingFooter" */
+import { useState } from "react"; 
+/* import LikedPodcasts from "./Components/GlobalComponents/LikedPodcasts";
+import RecommendedPodcasts from "./Components/GlobalComponents/RecommendedPodcasts";
+import LoadingFooter from "./Components/Loading/LoadingFooter" */
 /* import { useState, useRef } from "react"; */
 
 function App() {
+  const [currentEpisode, setCurrentEpisode] = useState(null);
   return (
     <div className="flex flex-col min-h-screen">
       <PodcastHeader />
 
       <main className="pt-20 pb-24">
-        <Library />
+        <div>
+          <Library onEpisodeSelect={setCurrentEpisode} />
+        </div>
       </main>
-      <PodcastFooter />
+      <div className="min-h-screen pb-24">
+          {/* content */}
+          <PodcastFooter episode={currentEpisode} />
+      </div>
     </div>
   );
 }
